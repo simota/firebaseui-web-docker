8fce04cef34ee49d8a640480af4ae867725b9ae3
simota <simota@me.com>
2018-04-05 10:02:00 +0900

    Add npm module

--- build.sh
+++ build.sh
@@ -7,4 +7,6 @@ fi
 docker build -t firebaseui-web-build .
 git clone https://github.com/firebase/firebaseui-web.git
 
-docker run -v `pwd`/firebaseui-web:/work -w /work --rm -it firebaseui-web-build:latest /bin/bash -c "npm install && npm run build build-js-${LANGUAGE_CODE}"
+docker run -v `pwd`/firebaseui-web:/work \
+	-w /work --rm -it firebaseui-web-build:latest \
+	/bin/bash -c "npm install && npm run build build-js-${LANGUAGE_CODE} && npm run build build-npm-${LANGUAGE_CODE}"
